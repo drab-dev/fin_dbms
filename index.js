3cf6e130fb079bc95b078ff8ca4d5a446f4cb257
Johan K George <reubynngeorge7x@gmail.com>
2025-06-07 01:29:33 +0530

    created fin.css for vite (vercel deployment)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Root from './Root';
+import Root from './Root.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
